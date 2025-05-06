99b2fd144cab04e3e6d13a774c885e13a8cc597f
FILIPRAIDER <filipraider123@gmail.com>
2025-05-06 11:30:45 -0500

    Correccion peticiones a prisma

--- src/actions/product/get-product-by-slug.ts
+++ src/actions/product/get-product-by-slug.ts
@@ -1,21 +1,27 @@
-"use server";
-
 import { ProductoClient } from "@/interfaces";
 import { prisma } from "@/lib/prisma";
 
 export const getProductBySlug = async (
   slug: string
 ): Promise<ProductoClient | null> => {
-  const p = await prisma.productos.findFirst({
-    where: { nombre: slug },
-    include: { imagenes: true },
-  });
+  try {
+    const p = await prisma.productos.findFirst({
+      where: { nombre: slug },
+      include: { imagenes: true },
+    });
 
-  if (!p) return null;
+    if (!p) return null;
 
-  return {
-    ...p,
-    id: p.id.toString(), // bigint → string
-    precio: Number(p.precio), // Decimal → number
-  };
+    return {
+      ...p,
+      id: p.id.toString(), // bigint → string
+      precio: Number(p.precio), // Decimal → number
+    };
+  } catch (error) {
+    console.error("Error al obtener el producto:", error);
+    return null;
+  } finally {
+    // Asegúrate de desconectar después de la consulta
+    await prisma.$disconnect();
+  }
 };

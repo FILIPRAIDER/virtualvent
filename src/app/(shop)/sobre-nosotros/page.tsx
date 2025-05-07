acfefddcec97d9124db07ccdf0e8aeec19f9b25d
i-am-tian <sebastian.alvarezla@campusucc.edu.co>
2025-05-07 16:21:33 -0500

    sobre nosotros actualizado

--- src/app/(shop)/sobre-nosotros/page.tsx
+++ src/app/(shop)/sobre-nosotros/page.tsx
@@ -4,17 +4,10 @@ export default function AboutUsPage() {
   return (
     <div>
       <Image
-        src="/imgs/Frame12.png"
-        width={402}
-        height={248}
-        className="w-screen hidden md:block object-cover"
-        alt="Equipo de trabajo"
-      />
-      <Image
-        src="/imgs/AboutUs.webp"
-        width={402}
-        height={248}
-        className="w-screen md:hidden object-cover"
+        src="/imgs/nosotros.jpg"
+        width={4000}
+        height={300}
+        className="w-[100%] md:block"
         alt="Equipo de trabajo"
       />
 
@@ -22,14 +15,9 @@ export default function AboutUsPage() {
         <h1 className="poppins font-bold text-black text-3xl">
           Sobre Nosotros
         </h1>
-        <p className="poppins text-[#575757] text-sm mt-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        <p className="poppins text-[#575757] text-sm mt-4 text-left max-w-[70ch]">
+          VirtualVent es una iniciativa digital solidaria liderada por jóvenes de la Universidad Cooperativa de Colombia en articulación con TransDigitalCoop.
+          Nuestro propósito es acortar la brecha entre los pequeños productores  y los consumidores, a través de plataformas digitales colaborativas que promueven el comercio justo. Somos una comunidad comprometida con la transformación social, la soberanía alimentaria y la economía solidaria, generando circuitos económicos justos mediante el uso estratégico de la tecnología.
         </p>
       </article>
       <section className="flex flex-col w-full  py-6 px-4 bg-[#D9D9D9]">

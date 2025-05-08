76b73d118d55550be84d6322dde8415d2e73e5ba
FILIPRAIDER <filipraider123@gmail.com>
2025-05-07 23:59:41 -0500

    Manejo de llamadas a prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,7 +4,11 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error", "warn"], // Opcional: útil para debugging
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
